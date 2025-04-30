854068023f092f395e0d099da623e08728121713
Francesco Donnini <donnini.francesco00@gmail.com>
2025-04-30 11:49:05 +0200

    Aggiunta descrizione della classe

--- src/main/java/io/github/francescodonnini/data/ParseContext.java
+++ src/main/java/io/github/francescodonnini/data/ParseContext.java
@@ -3,6 +3,15 @@ package io.github.francescodonnini.data;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 
+/**
+ * Un record che contiene tutte le informazioni necessarie per effettuare il parsing di un file Java e costruire
+ * uno o più oggetti JavaClass da esso.
+ *
+ * @param commit identificatore del commit che ha modificato il file
+ * @param parent directory genitore che contiene il file o una sua directory
+ * @param path percorso relativo del file rispetto alla directory genitore
+ * @param time data e ora del commit
+ */
 public record ParseContext(String commit, Path parent, Path path, LocalDateTime time) {
     public Path getAbsolutePath() {
         return parent.resolve(path);
