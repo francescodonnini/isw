40dad2634408997f142c2ebb25a8621d2cef1493
Francesco Donnini <donnini.francesco00@gmail.com>
2025-05-25 12:19:57 +0200

    Refactoring Main

--- src/main/java/io/github/francescodonnini/Main.java
+++ src/main/java/io/github/francescodonnini/Main.java
@@ -11,9 +11,14 @@ import io.github.francescodonnini.jira.JiraIssueApi;
 import io.github.francescodonnini.jira.JiraReleaseApi;
 import io.github.francescodonnini.jira.JiraVersionApi;
 import io.github.francescodonnini.jira.RestApi;
+import io.github.francescodonnini.model.Issue;
+import io.github.francescodonnini.model.JavaClass;
+import io.github.francescodonnini.model.JavaMethod;
 import io.github.francescodonnini.model.Release;
 import io.github.francescodonnini.proportion.Incremental;
 import io.github.francescodonnini.utils.FileUtils;
+import io.github.francescodonnini.workflow.Node;
+import io.github.francescodonnini.workflow.WorkflowBuilder;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -24,63 +29,106 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
-    private static String dataPath;
-    private static RestApi restApi = new RestApi();
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    private static boolean useCache;
+    private static double dropFactor;
+    private static String projectName ;
+    private static Path dataPath;
+    private static String projectPath;
+    private static String reportsPath;
+    private static final RestApi restApi = new RestApi();
+    private static CsvJavaMethodApi localMethodApi;
+    private static List<Release> releases;
+    private static List<JavaClass> classes;
+    private static List<JavaMethod> methods;
+    private static List<Issue> issues;
 
-    public static void main(String[] args) throws ConfigurationException, IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException, GitAPIException {
-        if (args.length != 2) {
-            System.exit(-1);
+    private static boolean loadReleases() {
+        var localVersionApi = new CsvVersionApi(dataPath.resolve("versions.csv").toString());
+        var jiraVersionApi = new JiraVersionApi(projectName, restApi);
+        var versionApi = new VersionRepository(jiraVersionApi, localVersionApi, false);
+        var jiraReleaseApi = new JiraReleaseApi(versionApi);
+        var releaseApi = new ReleaseRepository(jiraReleaseApi, new CsvReleaseApi(dataPath.resolve("releases.csv").toString()), false);
+        var allReleases = releaseApi.getReleases();
+        if (dropFactor > 1 || dropFactor < 0) {
+            throw new IllegalArgumentException("drop factor must be between 0 and 1");
         }
-        var projectName = args[1].toUpperCase();
-        // regex "<project name>-d+" ("%s-\\d+") è presente in tutti i commit che chiudono un ticket di JIRA
-        var settings = new IniSettings(args[0]);
-        var useCache = false;
-        var reportsPath = Path.of(settings.getString("pmdReportsPath"), projectName).toString();
-        FileUtils.createDirectory(reportsPath);
-        var sourcePath = Path.of(settings.getString("gitBasePath"), projectName.toLowerCase());
-        try (var git = createGit(Path.of(settings.getString("gitBasePath"), projectName.toLowerCase()))) {
-            var restApi = new RestApi();
-            var projectPath = Path.of(settings.getString("gitBasePath"), projectName.toLowerCase()).toString();
-            dataPath = Path.of(settings.getString("dataPath"), projectName).toString();
-            var releases = getReleases(projectName, 0.5, useCache);
-            var factory = new DataLoaderImpl(projectPath, new AbstractCounterFactoryImpl(), releases.getLast().releaseDate(), reportsPath.toString());
-            var localClassApi = new CsvJavaClassApi(Path.of(dataPath, "classes.csv").toString());
-            var classApi = new JavaClassRepository(factory, localClassApi, true);
-            var classes = classApi.getClasses();
-            var localMethodApi = new CsvJavaMethodApi(Path.of(dataPath, "methods.csv").toString(), classes);
-            var methods = localMethodApi.getLocal();
-            System.out.printf("retrieved %d methods%n", methods.size());
-            var codeSmellLinker = new CsvSmellLinker(reportsPath);
-            codeSmellLinker.link(classes);
-            var diff = new DiffCollector(releases, methods);
-            var diffed = diff.collect();
+        var remainingReleases = (1 - dropFactor) * allReleases.size();
+        releases = allReleases.subList(0, (int) remainingReleases);
+        return true;
+    }
+
+    private static boolean loadProgramData() {
+        try {
+            var dataLoader = new DataLoaderImpl(projectPath, new AbstractCounterFactoryImpl(), releases.getLast().releaseDate(), reportsPath);
+            var localClassApi = new CsvJavaClassApi(dataPath.resolve("classes.csv").toString());
+            var classApi = new JavaClassRepository(dataLoader, localClassApi, useCache);
+            classes = classApi.getClasses();
+            localMethodApi = new CsvJavaMethodApi(dataPath.resolve("methods.csv").toString(), classes);
+            var methodApi = new JavaMethodRepository(dataLoader, localMethodApi, useCache);
+            methods = methodApi.getMethods();
+            return true;
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "@loadProgramData: got error {}", e.getMessage());
+            return false;
+        }
+    }
+
+    private static boolean linkCodeSmells() {
+        var linker = new CsvSmellLinker(reportsPath);
+        linker.link(classes);
+        try {
+            localMethodApi.saveLocal(methods, dataPath.resolve("methods-with-smells.csv").toString());
+            return true;
+        } catch (IOException | CsvRequiredFieldEmptyException | CsvDataTypeMismatchException e) {
+            logger.log(Level.SEVERE, "@linkCodeSmells: got error {}", e.getMessage());
+            return false;
+        }
+    }
+
+    private static boolean collectChangeMetrics() {
+        var collector = new DiffCollector(releases, methods);
+        methods = collector.collect();
+        try {
+            localMethodApi.saveLocal(methods, dataPath.resolve("methods-diffed.csv").toString());
+            return true;
+        } catch (IOException | CsvRequiredFieldEmptyException | CsvDataTypeMismatchException e) {
+            logger.log(Level.SEVERE, "@collectChangeMetrics: got error {}", e.getMessage());
+            return false;
+        }
+    }
+
+    private static boolean doProportion() {
+        try {
+            var commits = getCommits(projectPath);
+            var pattern = "%s-\\d+".formatted(projectName);
+            var jiraIssueApi = new JiraIssueApi(projectName, pattern, restApi, releases, commits);
+            var localIssueApi = new CsvIssueApi(dataPath.resolve("issues.csv").toString(), releases, commits);
+            var issueApi = new IssueRepository(jiraIssueApi, localIssueApi, useCache);
+            issues = issueApi.getIssues();
+            var proportion = new Incremental(issues, releases);
+            proportion.makeLabels();
+            return true;
+        } catch (IOException | GitAPIException e) {
+            logger.log(Level.SEVERE, "@doProportion: got error {}", e.getMessage());
+            return false;
+        }
+    }
+
+    private static List<RevCommit> getCommits(String projectPath) throws IOException, GitAPIException {
+        try (var git = createGit(projectPath)) {
             var commits = new ArrayList<RevCommit>();
             git.log().call().forEach(commits::add);
-            var localIssueApi = new CsvIssueApi(Path.of(dataPath, "issues.csv").toString(), releases, commits);
-            var pattern = "%s-\\d+".formatted(projectName);
-            var remoteIssueApi = new JiraIssueApi(projectName, pattern, restApi, releases, commits);
-            var issueApi = new IssueRepository(remoteIssueApi, localIssueApi, useCache);
-            var issues = issueApi.getIssues();
-            System.out.printf("retrieved %d issues%n", issues.size());
-            System.out.printf("issues with affectedVersions" + issues.stream().filter(i -> i.affectedVersions().size() > 0).count());
-            var proportion = new Incremental(issues, releases);
-            var finalIssues = proportion.makeLabels();
-            localIssueApi.saveLocal(finalIssues, Path.of(dataPath, "issues-with-prop.csv").toString());
-            var labelMaker = new LabelMakerImpl(git, finalIssues, diffed, releases);
-            var labeledMethods = labelMaker.makeLabels();
-            localMethodApi.saveLocal(labeledMethods, Path.of(dataPath, "lbl-with-prop-methods.csv").toString());
-            System.out.println("number of buggy methods: " + labeledMethods.stream().filter(m -> m.isBuggy()).count());
+            return commits;
         }
     }
 
-    private static Path createReportsPath(String reportsBaseDir) {
-        return Path.of(reportsBaseDir, String.valueOf(System.currentTimeMillis()));
-    }
-
-    private static Git createGit(Path projectPath) throws IOException {
-        var gitPath = projectPath.resolve(".git");
+    private static Git createGit(String projectPath) throws IOException {
+        var gitPath = Path.of(projectPath, ".git");
         try (var repository = new FileRepositoryBuilder()
                 .setGitDir(gitPath.toFile())
                 .build()) {
@@ -88,35 +136,47 @@ public class Main {
         }
     }
 
-    private static List<Release> getReleases(String projectName, double dropFactor, boolean useCache) {
-        var releaseApi = getReleaseApi(projectName, restApi, dataPath, useCache);
-        var releases = releaseApi.getReleases();
-        var lastRelease = Math.min(((int) (releases.size() * dropFactor)) + 1, releases.size());
-        return releases.subList(0, lastRelease + 1);
-    }
-
-    private static ReleaseApi getReleaseApi(String projectName, RestApi restApi, String localDataPath, boolean useCache) {
-        var versionApi = getVersionApi(projectName, restApi, localDataPath, useCache);
-        var remoteReleaseApi = new JiraReleaseApi(versionApi);
-        var localReleaseApi = new CsvReleaseApi(Path.of(localDataPath, "releases.csv").toString());
-        return new ReleaseRepository(remoteReleaseApi, localReleaseApi, useCache);
-    }
-
-    private static VersionApi getVersionApi(String projectName, RestApi restApi, String localDataPath, boolean useCache) {
-        var remoteVersionApi = new JiraVersionApi(projectName, restApi);
-        var localVersionApi = new CsvVersionApi(Path.of(localDataPath, "versions.csv").toString());
-        return new VersionRepository(remoteVersionApi, localVersionApi, useCache);
-    }
-
-    private static List<RevCommit> getCommits(String projectPath) throws IOException, GitAPIException {
-        var gitPath = Path.of(projectPath, ".git");
-        try (var repository = new FileRepositoryBuilder()
-                .setGitDir(gitPath.toFile())
-                .build();
-            var git = new Git(repository);) {
-            var commits = new ArrayList<RevCommit>();
-            git.log().call().forEach(commits::add);
-            return commits;
+    private static boolean doLabelling() {
+        try (var git = createGit(projectPath)) {
+            var labelMaker = new LabelMakerImpl(git, issues, methods, releases);
+            methods = labelMaker.makeLabels();
+            localMethodApi.saveLocal(methods, dataPath.resolve("methods-labeled.csv").toString());
+            return true;
+        } catch (IOException | CsvRequiredFieldEmptyException | CsvDataTypeMismatchException e) {
+            logger.log(Level.SEVERE, "@doLabelling: got error {}", e.getMessage());
+            return false;
         }
+    }
+
+    public static void main(String[] args) throws ConfigurationException, IOException {
+        init(args);
+        completeWorkflow();
+    }
+
+    private static void init(String[] args) throws ConfigurationException, IOException {
+        if (args.length != 2) {
+            System.exit(-1);
+        }
+        projectName = args[1].toUpperCase();
+        // regex "<project name>-d+" ("%s-\\d+") è presente in tutti i commit che chiudono un ticket di JIRA
+        var settings = new IniSettings(args[0]);
+        useCache = false;
+        dropFactor = settings.getDouble("dropFactor");
+        reportsPath = Path.of(settings.getString("pmdReportsPath"), projectName).toString();
+        FileUtils.createDirectory(reportsPath);
+        projectPath = Path.of(settings.getString("gitBasePath"), projectName.toLowerCase()).toString();
+        dataPath = Path.of(settings.getString("dataPath"), projectName);
+    }
+
+    private static void completeWorkflow() {
+        new WorkflowBuilder()
+                .addNode(Node.create("1", "loadReleases", Main::loadReleases))
+                .addNode(Node.create("2", "loadProgramData", List.of("1"), Main::loadProgramData))
+                .addNode(Node.create("3", "linkCodeSmells", List.of("2"), Main::linkCodeSmells))
+                .addNode(Node.create("4", "collectChangeMetrics", List.of("3"), Main::collectChangeMetrics))
+                .addNode(Node.create("5", "doProportion", List.of("4"), Main::doProportion))
+                .addNode(Node.create("6", "doLabelling", List.of("5"), Main::doLabelling))
+                .build()
+                .execute();
     }
 }
