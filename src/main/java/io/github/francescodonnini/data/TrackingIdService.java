d886ddf15805c870f7bd10fc3d05b1233bd6b842
francesco <donnini.francesco00@gmail.com>
2026-01-14 14:44:48 +0100

    rimossi log inutili

--- src/main/java/io/github/francescodonnini/data/TrackingIdService.java
+++ src/main/java/io/github/francescodonnini/data/TrackingIdService.java
@@ -8,7 +8,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TrackingIdService {
-    private final Logger logger = Logger.getLogger(TrackingIdService.class.getName());
     private final AtomicLong id = new AtomicLong(0);
     private final Map<Path, Long> map = new ConcurrentHashMap<>();
 
@@ -20,12 +19,10 @@ public class TrackingIdService {
         var oldId = map.get(oldPath);
         var newId = map.get(newPath);
         if (oldId != null) {
-            logger.log(Level.INFO, "UPDATE %d FOR %s -> %s".formatted(oldId, oldPath, newPath));
             map.remove(oldPath);
             map.put(newPath, oldId);
         }
         if (newId != null) {
-            logger.log(Level.INFO, "ALREADY EXISTS %d FOR %s".formatted(newId, newPath));
             return;
         }
         getId(newPath);
