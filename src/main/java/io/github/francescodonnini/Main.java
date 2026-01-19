f8cea99692d9a492d4bf6128f73ade4f68c5d35c
Francesco Donnini <donnini.francesco00@gmail.com>
2026-01-19 23:23:26 +0100

    aggiunti controlli per il passaggio di parametri da linea di comando

--- src/main/java/io/github/francescodonnini/Main.java
+++ src/main/java/io/github/francescodonnini/Main.java
@@ -5,17 +5,23 @@ import io.github.francescodonnini.cli.FeatureSelectionCli;
 import io.github.francescodonnini.cli.MLCli;
 import picocli.CommandLine;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        if (args.length <= 1) {
+            throw new IllegalArgumentException("expected at least two arguments but got " + args.length);
+        }
+        var slice = Arrays.copyOfRange(args, 1, args.length);
         switch (args[0]) {
             case "data":
-                new CommandLine(new DataCli()).execute(args);
+                new CommandLine(new DataCli()).execute(slice);
                 break;
             case "feature_selection":
-                new CommandLine(new FeatureSelectionCli()).execute(args);
+                new CommandLine(new FeatureSelectionCli()).execute(slice);
                 break;
             case "ml":
-                new CommandLine(new MLCli()).execute(args);
+                new CommandLine(new MLCli()).execute(slice);
                 break;
             default:
                 throw new IllegalArgumentException("Unknown option: " + args[0]);
