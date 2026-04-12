e2700a7ceb8fc88e7c0c7d98fceb7a28065591bf
francesco <donnini.francesco00@gmail.com>
2026-04-12 15:08:02 +0200

    stampe per statistiche

--- src/main/java/io/github/francescodonnini/pipeline/data/LoadProjectInfoStep.java
+++ src/main/java/io/github/francescodonnini/pipeline/data/LoadProjectInfoStep.java
@@ -4,7 +4,11 @@ import io.github.francescodonnini.pipeline.inputs.DataPipelineContext;
 import io.github.francescodonnini.pipeline.inputs.ProjectInfo;
 import io.github.francescodonnini.pipeline.Step;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class LoadProjectInfoStep implements Step<ProjectInfo, ProjectInfo> {
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
     private final DataPipelineContext context;
 
     public LoadProjectInfoStep(DataPipelineContext context) {
@@ -15,8 +19,12 @@ public class LoadProjectInfoStep implements Step<ProjectInfo, ProjectInfo> {
     public ProjectInfo execute(ProjectInfo input) {
         var releases = context.getReleaseApi()
                 .getReleases(input.getProject());
+        logger.log(Level.INFO, "Total number of releases: {0}", releases.size());
         var issues = context.getIssueApi()
                 .getIssues(input.getProject());
+        final var avPresent = issues.stream().filter(i -> !i.affectedVersions().isEmpty()).count();
+        logger.log(Level.INFO, "Total number of issues: {0}", issues.size());
+        logger.log(Level.INFO, "AV%:                    {0}", (double)avPresent / issues.size());
         input.setIssues(issues);
         input.setAllReleases(releases);
         return input;
