2a4689beb0943885dea36e0c1cf2d3911cd9ebd7
francesco <donnini.francesco00@gmail.com>
2026-05-01 17:50:01 +0200

    unused imports

--- src/main/java/io/github/francescodonnini/collectors/ast/AbstractCounter.java
+++ src/main/java/io/github/francescodonnini/collectors/ast/AbstractCounter.java
@@ -1,11 +1,7 @@
 package io.github.francescodonnini.collectors.ast;
 
-import com.sun.source.tree.ClassTree;
 import com.sun.source.util.TreeScanner;
 import io.github.francescodonnini.model.RevisionJavaClass;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public abstract class AbstractCounter extends TreeScanner<Void, RevisionJavaClass> {
     public abstract void reset();
